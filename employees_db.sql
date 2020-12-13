4870ec7355ed436c05fbef0590a36b8942d5db11
Kyle Pykonen <kylepyk@gmail.com>
2020-12-12 23:17:18 -0800

    Filled out CREATE TABLE employee

--- employees_db.sql
+++ employees_db.sql
@@ -11,5 +11,13 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30),
-    salary  
+    salary DECIMAL (10, 2) 
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT(15),
+    manager_id INT(15)
 )
