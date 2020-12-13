86d1d1d957d4f82d53469673519664c95b940990
Kyle Pykonen <kylepyk@gmail.com>
2020-12-12 16:46:23 -0800

    began filling out CREATE TABLE role and corrected varchar numbers

--- employees_db.sql
+++ employees_db.sql
@@ -4,10 +4,12 @@ USE employees_db;
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(50),
+    name VARCHAR(30),
     department_id INT(20)
 );
 
 CREATE TABLE role (
-    
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30),
+    salary  
 )
